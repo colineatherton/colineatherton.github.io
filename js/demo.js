5e4f81149676799a3951736a04c5d32801f85b1b
Colin <colin@energydeck.com>
2015-06-18 10:11:25 +0100

    removed

--- js/demo.js
+++
@@ -1,7 +0,0 @@
-     $(document).ready(function(){
-        var demoCTA = window.location.hash;
-        if (demoCTA == '#demo') {
-        	$('#caseStudy1').removeClass('active');
-        	$('#demos').addClass('active');
-        };
-      });
