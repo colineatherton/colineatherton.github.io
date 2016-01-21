32b50c4442040642911b6a55f1c352a16d8e9ef7
Colin <colin@energydeck.com>
2016-01-21 13:38:58 +0000

    added learnD3.js

---
+++ d3/js/learnD3.js
@@ -0,0 +1,6 @@
+d3.select("#someDiv").style("border", "5px darkgrey dashed");
+// style for appearance
+d3.select("#someDiv").attr("id", "newID");
+// attribute for id and class
+d3.select("#someCheckbox").property("checked", true);
+// property for state
